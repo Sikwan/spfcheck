4e8058291d12bdb759d1425126896ca708f0747b
Francois Vanderkelen <francois.vanderkelen@experian.com>
2013-10-01 02:04:20 -0700

    0.1.2dev

--- README.txt
+++ README.txt
@@ -1,11 +1,13 @@
 SPFChecker
 
-version: 0.1b
+version: 0.1.2dev
 
 Description:
 This package is used to validate SPF or SenderID for your emails. It is your job to gather the sending domain and ip,
 once this is done you can use this package to validate it.
 
 Usage:
+
 - Instatiate the class sikwan.spfchecker.SPFCheck (You can pass a logger to the class if you want verbose mode)
+
 - Use SPFCheck.verify(ip, domain, version). (Note that version is by default to 1 for SPF, use 2 for SenderId)
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-version = "0.1b"
+version = "0.1.2dev"
 
 README = os.path.join(os.path.dirname(__file__), 'README.txt')
 long_description = open(README).read() + '\n\n'
@@ -13,15 +13,16 @@ setup(name="sikwan.spfcheck",
         install_requires=["dnspython", "netaddr"],
         keywords="sikwan spf spfcheck",
         author="Sikwan",
+        author_email="vanderkelen.francois@gmail.com",
         url="https://github.com/Sikwan/spfcheck",
         license="GPL",
         packages=find_packages(),
         namespace_packages=['sikwan'],
         classifiers = [
-            'Development Status :: 1 - Dev',
+            'Development Status :: 3 - Alpha',
             'Intended Audience :: Developers',
             'Intended Audience :: System Administrators',
-            'License :: OSI Approved :: GNU Gneral Public License (GPL)',
+            'License :: OSI Approved :: GNU General Public License (GPL)',
             'Operating System :: Unix',
             'Programming Language :: Python',
             'Topic :: Software Development :: Libraries :: Python Modules',
